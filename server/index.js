b97513f9646a8ea5f36a177248821804a1fe9b03
Martin Pernecky <mpe@auctionata.com>
2017-02-17 18:31:15 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 var express = require('express'),
-  app = express()
+  app = express();
 
-app.get('/', function(req, res) {
-  res.json(process.env);
+app.get('/', function(req, res, next) {
+  res.json({
+    env: process.env,
+    headers: req.headers
+  });
 });
 
 app.get('/pid', function(req, res) {
@@ -13,4 +16,5 @@ app.get('/:n', function(req, res) {
   res.send(200, process.env[req.params.n]);
 });
 
-app.listen(8080);
+app.listen(3000, '0.0.0.0');
+console.log('running');
